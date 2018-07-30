cff9f3cdc886c29e08c88e54aeacba9dec355122
EdytaKarpinska <edyta@sgrouples.com>
2018-07-30 19:44:27 +0200

    test_add_group - lesson 1 - homework part 1

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 
+
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -10,11 +11,12 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_add_group(unittest.TestCase):
+
+class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
-    
+
     def test_test_add_group(self):
         success = True
         wd = self.wd
@@ -39,8 +41,6 @@ class test_add_group(unittest.TestCase):
         ActionChains(wd).move_to_element(wd.find_element_by_xpath("//form[@id='LoginForm']//label[.='Password:']")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_id("LoginForm")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]")).perform()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         ActionChains(wd).move_to_element(wd.find_element_by_id("content")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_css_selector("hr")).perform()
@@ -68,8 +68,6 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("Python Group")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").send_keys("\\9")
         ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']/form")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_name("group_header")).perform()
         wd.find_element_by_name("group_header").click()
@@ -77,55 +75,9 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_header").send_keys("This is the Logo")
         ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']/form")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_name("group_footer")).perform()
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").send_keys("\\undefined")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("Here we have a group footer")
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']/form")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_name("submit")).perform()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").send_keys("\\undefined")
-        wd.find_element_by_name("submit").click()
-        ActionChains(wd).move_to_element(wd.find_element_by_css_selector("html")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_css_selector("html")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_css_selector("html")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_id("content")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_name("user")).perform()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        ActionChains(wd).move_to_element(wd.find_element_by_id("LoginForm")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//form[@id='LoginForm']//label[.='Password:']")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_id("LoginForm")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]")).perform()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']//form[normalize-space(.)='']")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_name("new")).perform()
-        wd.find_element_by_name("new").click()
-        ActionChains(wd).move_to_element(wd.find_element_by_css_selector("label")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']/form")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_name("group_name")).perform()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Python group")
-        ActionChains(wd).move_to_element(wd.find_element_by_name("group_header")).perform()
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Header")
-        ActionChains(wd).move_to_element(wd.find_element_by_name("group_footer")).perform()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer")
-        ActionChains(wd).move_to_element(wd.find_element_by_xpath("//div[@id='content']/form")).perform()
-        ActionChains(wd).move_to_element(wd.find_element_by_name("submit")).perform()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").send_keys("\\undefined")
         wd.find_element_by_name("submit").click()
         ActionChains(wd).move_to_element(wd.find_element_by_css_selector("html")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_id("footer")).perform()
@@ -143,9 +95,10 @@ class test_add_group(unittest.TestCase):
         ActionChains(wd).move_to_element(wd.find_element_by_id("header")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_css_selector("html")).perform()
         self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
