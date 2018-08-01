9d9a82bfcd9c1a40031fe44d97f6e01053e4dafc
EdytaKarpinska <edyta@sgrouples.com>
2018-08-01 21:11:44 +0200

    lesson 1 - homework part 3 - add contact

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+    def __init__(self, userfirstname, usermiddlename, userlastname, nick, title, company, address, home,
+                mobile, workphone, fax, email, email2, email3, homepage, birthyear, anniversaryyear, secondaddres,
+                homephone, notes):
+        self.userfirstname = userfirstname
+        self.usermiddlename = usermiddlename
+        self.userlastname = userlastname
+        self.nick = nick
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.workphone = workphone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birthyear = birthyear
+        self.anniversaryyear = anniversaryyear
+        self.secondaddres = secondaddres
+        self.homephone = homephone
+        self.notes = notes
