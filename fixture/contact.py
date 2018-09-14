87b360e549d5c69671820f7959631361cc531db8
EdytaKarpinska <edyta@sgrouples.com>
2018-09-14 20:11:48 +0200

    lesson 4 - homework 12 - cashing

--- fixture/contact.py
+++ fixture/contact.py
@@ -20,6 +20,7 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.open_return_contacts_page()
+        self.contact_cache = None
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
@@ -69,7 +70,7 @@ class ContactHelper:
         wd.find_element_by_xpath("/html/body/div/div[4]/form[2]/div[2]/input").click()
         wd.switch_to.alert.accept()
         self.open_return_contacts_page()
-
+        self.contact_cache = None
 
     def editing_first_contact(self, new_contact_data):
         wd = self.app.wd
@@ -81,19 +82,22 @@ class ContactHelper:
         #wd.switch_to.alert.accept()
         wd.find_element_by_name("update").click()
         self.open_return_contacts_page()
-
+        self.contact_cache = None
 
     def count_c(self):
         wd = self.app.wd
         self.open_return_contacts_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.open_return_contacts_page()
-        contacts = []
-        for element in wd.find_elements_by_name("entry"):
-            text = element.find_element_by_xpath("td[3]").text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(userfirstname=text, id=id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_return_contacts_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                text = element.find_element_by_xpath("td[3]").text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(userfirstname=text, id=id))
+        return list(self.contact_cache)
--- test/add_contact.py
+++ test/add_contact.py
@@ -9,7 +9,8 @@ def test_add_contact(app):
                                mobile="789456123", workphone="85965784", fax="879654", email="test@localhost.com", email2="test2@localhost.com",
                                email3="test3@localhost.com", homepage="homepage", birthyear="1990", anniversaryyear="2015", secondaddres="Secondary address", homephone="Second home", notes="Note")
     app.contact.create_c(contact)
+    # do usuniecia new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == app.contact.count_c()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_maxc) == sorted(new_contacts, key=Contact.id_or_maxc)
